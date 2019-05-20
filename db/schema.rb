7e18f04c95f0a8bc20636088c9f0586bcde22d59
中山　卓紀 <tatsuking@nakayamatakukinoMacBook-Pro.local>
2019-05-20 16:08:05 +0900

    sort

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_184201) do
+ActiveRecord::Schema.define(version: 2019_05_19_175631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_05_17_184201) do
     t.string "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline_at"
   end
 
 end
